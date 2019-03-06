c8ea33ec89c7a4e63544419d6e75fb0c10f7e0e2
babylionjane <runinging@gmail.com>
2019-03-07 01:46:54 +0900

    드디어 ㅠㅠㅠ contrib.auth.loginview
    djdjjjslkdafalsdf

--- sgsite/urls.py
+++ sgsite/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
