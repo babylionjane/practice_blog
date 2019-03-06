bf89b100e7913c7cd4387fbddce1e02f55419d77
babylionjane <runinging@gmail.com>
2019-03-07 01:16:16 +0900

    on_delete 추가
    무ㅗㄹ라 ㅠㅠㅠTypeError: __init__() missing 1 required positional argument: 'on_delete'

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
+    post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE,)
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
