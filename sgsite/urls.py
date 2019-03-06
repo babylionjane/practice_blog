06a062f0281027a6a71753228d1e6f518213ff28
babylionjane <runinging@gmail.com>
2019-03-07 02:06:08 +0900

    update urls.py
    으악

--- sgsite/urls.py
+++ sgsite/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),   
     url(r'', include('blog.urls')),
 ]
